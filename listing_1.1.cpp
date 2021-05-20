477da344d0ccf40db785b875e4fcbd250b602ed7
zyj <2990657021@qq.com>
2021-05-20 23:03:00 +0800

    0520 night

--- listing_1.1.cpp
+++ listing_1.1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <thread>
 
+
+
+
+/*每个线程都必须有一个初始函数，新线程的执行从这里开始
+ * 对于应用程序，初试线程是main，但是对于线程t，开始于hello();
+ * */
+
+
 void hello()
 {
     std::cout<<"Hello Concurrent World\n";
@@ -10,4 +18,7 @@ int main()
 {
     std::thread t(hello);
     t.join();
+//    调用join（）函数，表示线程main会等待域线程相关的线程。
+    return 0;
+
 }
--- listing_2.1.cpp
+++ listing_2.1.cpp
@@ -1,7 +1,9 @@
 #include <thread>
-
+#include <iostream>
+using  namespace std;
 void do_something(int& i)
 {
+    cout<<i<<endl;
     ++i;
 }
 
@@ -13,7 +15,7 @@ struct func
 
     void operator()()
     {
-        for(unsigned j=0;j<1000000;++j)
+        for(unsigned j=0;j<100;++j)
         {
             do_something(i);
         }
@@ -26,7 +28,16 @@ void oops()
     int some_local_state=0;
     func my_func(some_local_state);
     std::thread my_thread(my_func);
-    my_thread.detach();
+
+    cout<<my_thread.joinable()<<endl;
+
+//    my_thread.detach();
+//    detach()表示进程分离
+    my_thread.join();
+    /* 这意味着， 只能对一个线程使用一次join();一旦已经使用过
+        join()， std::thread 对象就不能再次加入了， 当对其使
+     用joinable()时， 将返回否（ false） */
+    cout<<my_thread.joinable()<<endl;
 }
 
 int main()
